433e90256381a285fdf06c02b4f4b06cbc341cda
Khalid Belhadj <khalidbelhadj124@gmail.com>
2022-12-06 09:04:23 +0000

    Day 6 done

---
+++ 2022/Day-6/day_6.py
@@ -0,0 +1,16 @@
+def find_signal(file, marker_length):
+    for i in range(len(file) - marker_length):
+        s = file[i:i + marker_length]
+        if (len(s) == len(list(set(s)))):
+            return i + marker_length
+
+
+def main():
+    file = open('input.txt')
+    file = file.read()
+    print(f'Part 1: {find_signal(file, 4)}')
+    print(f'Part 2: {find_signal(file, 14)}')    
+
+
+if __name__ == '__main__':
+    main()
