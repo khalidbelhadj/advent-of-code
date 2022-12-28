2857755acb3f48736c43acff27d3badac639a5e2
khalidbelhadj <106029014+khalidbelhadj@users.noreply.github.com>
2022-12-28 20:20:40 +0000

    Day 16 (finally) done

--- 2022/day_16/day_16.py
+++ 2022/day_16/day_16.py
@@ -1,92 +1,52 @@
 import re
-from heapq import heappop, heappush
+from functools import cache
 
-valves: dict = {}
-distances: dict[str, dict] = {}
-valves_with_pressure: list = []
-TOTAL_TIME_1: int = 30
-
-def max_flow():
-    queue = [(0, 0, "AA", ("AA",))]
-    highest_pressure_per_path: dict = {}
-
-    while queue:
-        pressure_released, time_elapsed, current_valve, current_path = heappop(queue)
-
-        for val, dist in distances[current_valve].items():
+valves: dict[str, tuple[int, list[str]]] = {}
+TOTAL_TIME: int = 30
 
 
-            # 1 min for each unit dist +1 for valve opening time8
-            next_time_elapsed = time_elapsed + dist + 1
-
-            if val in current_path or next_time_elapsed > TOTAL_TIME_1: continue
-            
-            # Adding the total remaining time times the pressure released per minute from val
-            next_pressure_released = pressure_released + (TOTAL_TIME_1 - next_time_elapsed) * valves[val][0]
-
-
-            next_path = tuple(list(current_path) + [val])
-
-            if (next_path not in highest_pressure_per_path or 
-                highest_pressure_per_path[next_path] < next_pressure_released):
-                
-                highest_pressure_per_path[next_path] = next_pressure_released
-                heappush(queue, (next_pressure_released, next_time_elapsed, val, next_path))
-
-    return max(highest_pressure_per_path.values())
-
-
-def get_distances() -> None:
-    global distances
-
-    valves_with_pressure.append('AA')
-    for start_valve in valves_with_pressure:
-
-        distances_from_valve: dict = {}
-        queue: list = [(0, start_valve)]
-        visited: set = set(start_valve)
-        
-        while queue:
-            dist, valve = heappop(queue)
-            for v in valves[valve][1]: # v in list of connected valves
-
-                if v in visited: continue
-
-                visited.add(v)
-
-                if v in valves_with_pressure and v != start_valve:
-                    distances_from_valve[v] = dist + 1
-
-                heappush(queue, (dist + 1, v))
-        distances[start_valve] = dict(distances_from_valve)
+@cache
+def max_score(pos: str, time: int, opened: frozenset[str], with_elephant: bool = False) -> int:
+    if time == 0:
+        if with_elephant:
+            return max_score('AA', TOTAL_TIME - 4, opened)
+        return 0
+    time -= 1
+    score = max([max_score(new_pos, time, opened, with_elephant) for new_pos in valves[pos][1]])
+    if valves[pos][0] > 0 and pos not in opened:
+        new_opened: set[str] = set(opened)
+        new_opened.add(pos)
+        score = max(
+            score,
+            time * valves[pos][0] + max_score(pos, time, frozenset(new_opened), with_elephant)
+        )
+    return score
 
 
 def parse() -> None:
-    global valves, valves_with_pressure
+    global valves
 
     with open('input.txt', 'r') as file:
         lines = file.readlines()
         for line in lines:
             valve: str = line.split()[1]
-            rate: int = int(re.search('rate=(\d+);', line).group(1))
+            rate: int = int(re.search(r'rate=(\d+);', line).group(1))
+            connected: list[str]
             if 'valves' in line:
-                connected: list[str] = line.split(';')[1].removeprefix(' tunnels lead to valves ').split(',')
+                connected = line.split(';')[1].removeprefix(' tunnels lead to valves ').split(',')
             else:
-                connected: list[str] = line.split(';')[1].removeprefix(' tunnel leads to valve ').split(',')
+                connected = line.split(';')[1].removeprefix(' tunnel leads to valve ').split(',')
 
             connected = list(map(lambda x: x.replace(' ', ''), connected))
             connected = list(map(lambda x: x.replace('\n', ''), connected))
             valves[valve] = (rate, connected)
 
-    valves_with_pressure = [v for v in valves if valves[v][0] > 0]
-
 
 def main() -> None:
     parse()
-    get_distances()
+    print(f'Part 1: {max_score("AA", TOTAL_TIME, frozenset())}')
+    print(f'Part 2: {max_score("AA", TOTAL_TIME - 4, frozenset(), True)}')
 
-    print(f'Part 1: {max_flow()}')
-    
 
 
 if __name__ == '__main__':
