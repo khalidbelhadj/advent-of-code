8c9fb227cf8914642393876d99c9c716f1957795
Khalid <Khalidbelhadj124@gmail.com>
2024-12-01 11:09:19 +0000

    day01

--- 2024/utils.cpp
+++ 2024/utils.cpp
@@ -7,13 +7,21 @@
 
 std::string read_to_string(std::string const& file_name) {
   std::ifstream file_stream(file_name);
+  if (file_stream.fail() | file_stream.bad()) {
+    std::cerr << "Failed to open file: " << file_name << std::endl;
+    exit(1);
+  }
   std::stringstream buffer;
   buffer << file_stream.rdbuf();
   return std::string(buffer.str());
 }
 
-std::vector<std::string> read_to_lines(std::string file_name) {
+std::vector<std::string> read_to_lines(const std::string& file_name) {
   std::ifstream file_stream(file_name);
+  if (file_stream.fail() | file_stream.bad()) {
+    std::cerr << "Failed to open file: " << file_name << std::endl;
+    exit(1);
+  }
   std::stringstream buffer;
   buffer << file_stream.rdbuf();
 
